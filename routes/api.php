fa307f83d6f1dbd35eb4354eaaf1c0d200c789bf
ieso1994 <ieso1994@live.com>
2018-12-24 18:20:27 +0800

    反反复复方法

--- routes/api.php
+++ routes/api.php
@@ -41,7 +41,7 @@ Route::group(['namespace' => 'Api'], function () {
 
 });
 
-Route::group(['middleware' => 'auth:api', 'namespace' => 'api'], function() {
+Route::group(['middleware' => 'auth:api', 'namespace' => 'Api'], function() {
 //Route::group(['middleware' => 'test', 'namespace' => 'api'], function () {
 
     Route::post('/putUserInfo', 'UserController@putUserInfo');//同步微信信息
