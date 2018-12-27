5fe22fa72b3ad024bbb491e4fd737e05c7e4a5ac
ieso1994 <ieso1994@live.com>
2018-12-27 16:54:35 +0800

    购物车 获取

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -9,36 +9,41 @@
 namespace App\Http\Controllers\Api;
 
 
+use App\Components\GoodsSPUManager;
 use App\Http\Controllers\Controller;
 use App\Http\Helpers\ApiResponse;
 use App\Models\Cart;
+use App\Models\GoodsSPU;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Request;
 
 class CartController extends Controller
 {
-	public static function myCart(Request $request)
-	{
+    public static function myCart(Request $request)
+    {
+        $carts = Auth::user()->carts;
+        foreach ($carts as $cart) {
+            $cart->spu = GoodsSPUManager::getDetailsForApp($cart->spu, $cart->sku_id);
+        }
+        return ApiResponse::makeResponse(true, $carts, ApiResponse::SUCCESS_CODE);
+    }
 
-		return ApiResponse::makeResponse(true,Auth::user()->carts,ApiResponse::SUCCESS_CODE);
-	}
-	
-	public static function addToCart(Request $request)
-	{
-		$cart = Cart::query()->firstOrCreate([
-			'user_id' => Auth::user()['id'],
-			'spu_id' => $request->spu_id,
-			'sku_id' => $request->sku_id,
-		]);
-		if (!$request->cancle) {
-			$cart->count = $request->count;
-			$cart->save();
-			$ret = '加入购物车成功';
-		} else {
-			$cart->delete();
-			$ret = '移出购物车成功';
-		}
-		return ApiResponse::makeResponse(true, $ret, ApiResponse::SUCCESS_CODE);
+    public static function addToCart(Request $request)
+    {
+        $cart = Cart::query()->firstOrCreate([
+            'user_id' => Auth::user()['id'],
+            'spu_id' => $request->spu_id,
+            'sku_id' => $request->sku_id,
+        ]);
+        if (!$request->cancle) {
+            $cart->count = $request->count;
+            $cart->save();
+            $ret = '加入购物车成功';
+        } else {
+            $cart->delete();
+            $ret = '移出购物车成功';
+        }
+        return ApiResponse::makeResponse(true, $ret, ApiResponse::SUCCESS_CODE);
 
-	}
+    }
 }
