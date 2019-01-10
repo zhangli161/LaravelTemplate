b12481591a7e28b5262c28e3af7ef29faa0b45f3
ieso1994 <ieso1994@live.com>
2019-01-10 14:39:55 +0800

    修复

--- app/Admin/Controllers/AgentController.php
+++ app/Admin/Controllers/AgentController.php
@@ -149,6 +149,8 @@ class AgentController extends Controller
 
     public function funsChartView($model, Request $request)
     {
+        if ($model->count==0)
+            return "还没有数据";
         $description = "";
         $datas = array();
         $type = $request->filled("type") ? $request->get("type") : 2;
@@ -212,7 +214,8 @@ class AgentController extends Controller
 
     public function orderChartView($model, Request $request)
     {
-
+        if ($model->count()==0)
+            return "还没有数据";
         $description = "";
         $datas = array();
         $type = $request->filled("type") ? $request->get("type") : 2;
