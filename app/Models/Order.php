bd8e5cd0d0adaecee32a7f7b38afd8d1ed0e89f4
ieso1994 <ieso1994@live.com>
2019-03-12 12:04:50 +0800

    111

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,39 +7,47 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-	protected $fillable = ['payment', 'payment_type', 'postage','post_fee', 'user_id','buyer_nick',"buyer_message",
-		'receiver_name','receiver_phone','receiver_region_id','receiver_address','note'];
+    protected $fillable = ['payment', 'payment_type', 'postage', 'post_fee', 'user_id', 'buyer_nick', "buyer_message",
+        'receiver_name', 'receiver_phone', 'receiver_region_id', 'receiver_address', 'note'];
     protected $appends = ['pay_value'];
-	public function user(){
-		return $this->belongsTo(User::class,'user_id');
-	}
-	public function skus()
-	{
-		return $this->hasMany(OrderSKU::class, 'order_id');
-	}
-	
-	public function coupon()
-	{
-		return $this->hasOne(OrderCoupon::class, 'order_id');
-	}
-	
-	public function wuliu()
-	{
-		return $this->hasOne(OrderPostage::class, 'order_id');
-	}
-	
-	public function xcx_pay(){
-		return $this->hasOne(WeiXinXPay::class,'order_id');
-	}
 
-	public function refund(){
-	    return $this->hasMany(OrderRefund::class,'order_id');
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 
-    public function order_agent(){
-	    return $this->hasOne(OrderAgent::class,"order_id");
+    public function skus()
+    {
+        return $this->hasMany(OrderSKU::class, 'order_id');
     }
-    public function getPayValueAttribute(){
-	    return $this->post_fee+$this->payment;
+
+    public function coupon()
+    {
+        return $this->hasOne(OrderCoupon::class, 'order_id');
+    }
+
+    public function wuliu()
+    {
+        return $this->hasOne(OrderPostage::class, 'order_id');
+    }
+
+    public function xcx_pay()
+    {
+        return $this->hasOne(WeiXinXPay::class, 'order_id');
+    }
+
+    public function refund()
+    {
+        return $this->hasMany(OrderRefund::class, 'order_id');
+    }
+
+    public function order_agent()
+    {
+        return $this->hasOne(OrderAgent::class, "order_id");
+    }
+
+    public function getPayValueAttribute()
+    {
+        return ($this->post_fee or 0) + ($this->payment or 0);
     }
 }
